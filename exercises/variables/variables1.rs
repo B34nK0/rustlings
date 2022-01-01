dc43d2d7c5a2a6b6a48be2148cb13b948e820925
B34nK0 <185392361@qq.com>
2022-01-01 14:56:43 +0000

    实践

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -6,9 +6,9 @@
 // even after you already figured it out. If you got everything working and
 // feel ready for the next exercise, remove the `I AM NOT DONE` comment below.
 
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
+    //变量需要使用let声明，赋值时已确定变量类型
+    //也可使用mut在变量名前声明变量为可变值,即后续使用时可被修改
+    let mut x = 5;
     println!("x has the value {}", x);
 }
--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,10 +1,10 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    //声明变量时需要确认变量类型，在1示例中，通过声明赋值方式确认了x的类型
+    //在变量名后使用:type的方式显示声明类型为u32即无符号32位值
+    let x:u32 = 10;
     if x == 10 {
         println!("Ten!");
     } else {
