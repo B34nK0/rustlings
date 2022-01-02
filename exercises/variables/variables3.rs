83291b310dbfccbd7189dca76f2ea2d591bb5f08
B34nK0 <185392361@qq.com>
2022-01-02 03:46:31 +0000

    不可变变量

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,10 +1,10 @@
 // variables3.rs
 // Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    //x是不可变，需要声明为mut
+    //不可变变量与常量有区别， 不可变变量可以被声明覆盖，而常量不能被覆盖
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
