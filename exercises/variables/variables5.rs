f4e9680318fec5086c08dea263e6d410b46f9679
B34nK0 <185392361@qq.com>
2022-01-02 03:54:14 +0000

    变量相关内容完结：let关键字 const关键字 mut 关键字。 变量的类型推导，cosnt常量需要显示声明类型，但实际开发中为了确认变量类型，最好采用显示声明类型

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,10 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
+    //rust支持shadowing，重影，所以可以重新加上let关键字 覆盖前一个number，重新声明的类型可从赋值值类型推导
+    let number = 3;
     println!("Number plus two is : {}", number + 2);
 }
