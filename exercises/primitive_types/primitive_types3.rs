881d98112f60d4768346ebb0fd3eecdad1b1c112
B34nK0 <185392361@qq.com>
2022-01-02 15:31:14 +0000

    基础类型、array以及tuple的初始化及访问

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,10 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    //初始化a为120个容量的数组，数组值均为10
+    let a = [10; 120];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
